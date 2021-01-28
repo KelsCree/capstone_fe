7434a0e6181ed8de5d890e03a3995ce9f11034d0
KelsCree <kelseycreehan@gmail.com>
2021-01-27 17:48:54 -0700

    Delete unused home code.

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect, useLayoutEffect } from 'react';
-import { StyleSheet, Text, View, Button, Modal } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import CreateAccount from './CreateAccount';
 import ViewCandidates from './ViewCandidates';
 import ViewOfficials from './ViewOfficials';
